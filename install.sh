046dc0c9695257afbfee263f2d3cf1e83d9b5300
Tech cochi (HYDRA) <61864331+rixon-cochi@users.noreply.github.com>
2021-06-27 10:15:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ pkg intall php -y
 pkg inatall curl -y
 clear
 sleep 1
-xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
+xdg-open https://www.youtube.com/channel/HYDRAGAMING4U
 echo " wait for 15 seconds "
 sleep 15
 echo " create account on cashkaro.com "
